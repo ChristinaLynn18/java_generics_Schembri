8ad3f6adefb2342e8fc2185f4d2ae437cd24620e
Christina <cls18@student.fdu.edu>
2020-05-13 19:28:28 -0400

    updated commit

--- src/csci3444/generics/MainEntry.java
+++ src/csci3444/generics/MainEntry.java
@@ -6,8 +6,6 @@ public class MainEntry {
 		MyGenInterface<Integer, String> mgi1 = new MyGenClass<Integer, String>(1, "Christina");
 		MyGenInterface<Integer, Integer> mgi2 = new MyGenClass<Integer, Integer>(1, 2017);
 		
-//		MyRegularClass.getSum(11, 12);
-		
 		Integer i1 = MyRegularClass.<Integer>getSum(10, 20);
 		Float f1 = MyRegularClass.<Float>getSum(100f, 200f);
 		
